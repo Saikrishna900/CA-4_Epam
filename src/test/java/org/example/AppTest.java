2a928c5f8bc4d2826c5fbc7654cfda6b60efe715
Saikrishna <vadlasaikrishna900@gmail.com>
2023-04-21 11:33:27 +0530

    Done

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,67 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+
+    ChromeDriver driver;
+
+    @BeforeClass
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+
+        driver.manage().window().maximize();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    void menu_list_1() {
+        Select select = new Select(driver.findElement(By.id("dropdowm-menu-1")));
+        select.selectByVisibleText("Python");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void menu_list_2() {
+        Select select = new Select(driver.findElement(By.id("dropdowm-menu-2")));
+        select.selectByVisibleText("TestNG");
+    }
+
+    @Test
+    void menu_list_3() throws InterruptedException {
+        Select select = new Select(driver.findElement(By.id("dropdowm-menu-3")));
+        select.selectByIndex(2);
+        Thread.sleep(2000);
+
+    }
+
+    @Test(priority = 1)
+    void check_box() throws InterruptedException {
+        WebElement checkbox2 = driver.findElement(By.cssSelector("input[value='option-2']"));
+        checkbox2.click();
+        WebElement checkbox3 = driver.findElement(By.cssSelector("input[value='option-3']"));
+        checkbox3.click();
+        checkbox3.click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    void radio_button() throws InterruptedException {
+        WebElement radio_button4 = driver.findElement(By.cssSelector("input[value='yellow']"));
+        radio_button4.click();
+        Thread.sleep(3000);
+    }
+
+    @AfterClass
+    void closeDriver() {
+        driver.close();
     }
 }
